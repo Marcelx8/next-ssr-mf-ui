394424e4007cf17349d8844661c99ad833431783
Marcelx8 <marcelsteenkamp3@gmail.com>
2022-02-11 23:15:46 +0200

    catchall dep update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,2 +1,14 @@
-import { createFederatedCatchAll } from "next-shared-logic";
-export default createFederatedCatchAll(process.env.REMOTES);
+import React from "react";
+import { createFederatedCatchAll } from "@module-federation/next-catchall";
+
+const ErrorComponent = () => {
+  return <h1>There was an error trying to load route</h1>;
+};
+const NotFoundComponent = () => {
+  return <h1>4OH4 not found</h1>;
+};
+export default createFederatedCatchAll(
+  process.env.REMOTES,
+  ErrorComponent,
+  NotFoundComponent
+);
